99955f1e2676c71db3b23ac1f464f021573345ae
worriedlemon <emilshteinberg+imprt@yandex.ru>
2024-02-27 18:58:28 +0300

    Rossler system reconstruction implemented

--- orthpoly/Test_Rossler.m
+++ orthpoly/Test_Rossler.m
@@ -12,11 +12,11 @@ ri = randi(size(y, 1), 1, N);
 rx = x(ri,:);
 ry = y(ri,:);
 
-figure(1);
+figure(1, "Position", [200 200 960 720]);
 plot3(x(:,1), x(:,2), x(:,3), "b", start_point(1), start_point(2), start_point(3), "r*");
 hold on;
 scatter3(rx(:,1), rx(:,2), rx(:,3), "k");
-title("Function reconstruction");
+xlabel("\itx"); ylabel("\ity"); zlabel("\itz");
 legend("Rossler attractor", "Start point", "Random points");
 
 fc = size(y, 2);
@@ -33,10 +33,22 @@ B = bernbase(tx', sigma);
 coefs = (B'*B)\B'*ry
 
 tx1 = x;
-y1 = y;
 for i = 1:fc
   tx1(:,i) = affine_transform(x(:,i), 0, 1, c(1,i), c(2,i));
-  y1(:,i) = bernbase(tx1', sigma) * coefs(:,i);
+end
+y1 = bernbase(tx1', sigma) * coefs;
+
+disp("Reconstructed system error:");
+err = norm(y - y1)
+
+function dx = sda(x, sigma, coefs, fc, c)
+  for i = 1:fc
+    tx1(:,i) = affine_transform((x')(:,i), 0, 1, c(1,i), c(2,i));
+  end
+  dx = transpose(bernbase(tx1', sigma) * coefs);
 end
 
-norm(y - y1)
+[~, x1] = ode45(@(t, x)sda(x, sigma, coefs, fc, c), 0:h:Tmax, start_point);
+
+plot3(x1(:,1), x1(:,2), x1(:,3), "-y", "DisplayName", "Reconstructed function");
+title(sprintf("Rossler system reconstruction\nError: %g", err));
